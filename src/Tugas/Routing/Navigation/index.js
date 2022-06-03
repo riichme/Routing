153d0aeb257d07387a021af16a4e5560d4fb38cf
ristmi <ristmidaffahoiri@gmail.com>
2022-06-03 21:02:27 +0700

    first komit

---
+++ src/Tugas/Routing/Navigation/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const Navigation =() => {
+return (
+    <div className="sidebar">
+        <a href="Home"> <i className="Home">Home</i></a>
+        <a href="Menu"> <i className="Menu">Home</i></a>
+        <a href="Contact"> <i className="Contact">Home</i></a>
+    </div>
+)
+}
+
+export default Navigation
---
+++ src/Tugas/Routing/Service/index.js
@@ -0,0 +1,10 @@
+import Navigation from "./Navigation"
+
+const Routing = () => {
+    return (
+        <div>
+            <Navigation/>
+        </div>
+    )
+}
+
